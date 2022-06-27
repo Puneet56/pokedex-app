0fb1cc5224c50fe7f403c7e5814d7e767d90d801
puneet <puneetdhiman56@gmail.com>
2022-06-27 18:18:23 +0530

    Pokedex structure

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar style={{ backgroundColor: "black" }} />
+			<View style={styles.pokedexContainer}>
+				<Image source={require("./assets/pokedex.jpg")} style={styles.pokedexImage}></Image>
+				<View style={styles.infoContainer}></View>
+			</View>
+		</View>
+	);
 }
 
+const pokedexColor = "#e62e2e";
+const bgColor = "#e53b3b";
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: "flex-start",
+		justifyContent: "flex-start",
+	},
+	pokedexContainer: {
+		position: "relative",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#e53b3b",
+		width: "100%",
+		height: "100%",
+	},
+	pokedexImage: {
+		flex: 1,
+		resizeMode: "contain",
+		justifyContent: "flex-start",
+		alignItems: "center",
+		width: "100%",
+		height: "100%",
+	},
+	infoContainer: {
+		position: "absolute",
+		top: "26%",
+		width: "82%",
+		aspectRatio: 1.01,
+		backgroundColor: "black",
+	},
 });
